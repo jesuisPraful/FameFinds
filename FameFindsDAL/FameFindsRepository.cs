0c37ae813011281508e2eff628b544c8b8e94940
jesuisPraful <prafusingh211@gmail.com>
2025-05-04 00:00:27 +0530

    Added ShopController functionality and logic updates

--- FameFindsDAL/FameFindsRepository.cs
+++ FameFindsDAL/FameFindsRepository.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace FameFindsDAL
 {
@@ -192,12 +193,12 @@ namespace FameFindsDAL
         }
         public bool AddProduct(Product product)
         {
-          var name= ( from p in _context.Products
-            where p.ProductName == product.ProductName
-            select p).FirstOrDefault();
+            var name = (from p in _context.Products
+                        where p.ProductName == product.ProductName
+                        select p).FirstOrDefault();
             bool status = false;
-            if ((name==null))
-            { 
+            if ((name == null))
+            {
                 try
                 {
                     _context.Products.Add(product);
@@ -263,8 +264,8 @@ namespace FameFindsDAL
             try
             {
                 product = (from p in _context.Products
-                          where p.ProductName == ProductName
-                          select p).FirstOrDefault();
+                           where p.ProductName == ProductName
+                           select p).FirstOrDefault();
             }
             catch (Exception ex)
             {
@@ -336,7 +337,7 @@ namespace FameFindsDAL
                     vendorObj.VendorName = vendor.VendorName;
                     _context.Vendors.Update(vendorObj);
                     _context.SaveChanges();
-                   status=true;
+                    status = true;
                 }
                 else
                 {
@@ -345,7 +346,7 @@ namespace FameFindsDAL
             }
             catch (Exception ex)
             {
-                status=false;
+                status = false;
             }
             return status;
         }
@@ -368,22 +369,19 @@ namespace FameFindsDAL
             }
             return false;
         }
-       
-
         #endregion
 
-    }
         #region Ratings
         public bool AddRating(Rating rating)
         {
             bool status = false;
             try
             {
-                var shopObj = context.Customers.Find(rating.ShopId);
+                var shopObj = _context.Customers.Find(rating.ShopId);
                 if (shopObj != null && rating.CustomerId != null)
                 {
-                    context.Ratings.Add(rating);
-                    context.SaveChanges();
+                    _context.Ratings.Add(rating);
+                    _context.SaveChanges();
                     status = true;
                 }
                 else
@@ -403,7 +401,7 @@ namespace FameFindsDAL
             List<Rating> ratings = new List<Rating>();
             try
             {
-                ratings = (from r in context.Ratings
+                ratings = (from r in _context.Ratings
                            select r).ToList();
             }
             catch (Exception ex)
@@ -418,12 +416,12 @@ namespace FameFindsDAL
             bool status = false;
             try
             {
-                var removeObj = context.Ratings.Find(ratingId);
+                var removeObj = _context.Ratings.Find(ratingId);
                 //if (removeObj != null && removeObj.CustomerId == rating.CustomerId)
-                if(removeObj != null && removeObj.CustomerId != null)
+                if (removeObj != null && removeObj.CustomerId != null)
                 {
-                    context.Ratings.Remove(removeObj);
-                    context.SaveChanges();
+                    _context.Ratings.Remove(removeObj);
+                    _context.SaveChanges();
                     status = true;
                 }
             }
@@ -435,5 +433,294 @@ namespace FameFindsDAL
         }
         #endregion
 
+        #region shop
+
+        //Register Shops
+        public bool RegisterShop(Shop shop)
+        {
+            bool status = false;
+            try
+            {
+                _context.Shops.Add(shop);
+                _context.SaveChanges();
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+
+
+        //Get All Shops 
+        public List<Shop> GetAllShops()
+        {
+
+            List<Shop> shops = new List<Shop>();
+            try
+            {
+                shops = _context.Shops.ToList();
+            }
+
+            catch (Exception ex)
+            {
+                shops = null;
+
+            }
+            return shops;
+        }
+
+
+        //Get Shop By ShopId
+        public Shop GetShopsByShopId(int shopId)
+        {
+
+            Shop shops = new Shop();
+            try
+            {
+                shops = _context.Shops
+                    .Where(s => s.ShopId == shopId).Select(s => s)
+                    .FirstOrDefault();
+            }
+
+            catch (Exception ex)
+            {
+                shops = null;
+
+            }
+            return shops;
+        }
+
+
+        //Get Shop by Vendor Id
+        public List<Shop> GetShopsByVendorId(int vendorId)
+        {
+
+            List<Shop> shops = new List<Shop>();
+            try
+            {
+                shops = _context.Shops
+                    .Where(s => s.VendorId == vendorId).Select(s => s)
+                    .ToList();
+            }
+
+            catch (Exception ex)
+            {
+                shops = null;
+
+            }
+            return shops;
+        }
+
+
+
+        //Get Shop by ShopName
+        public List<Shop> GetShopsByShopName(string shopName)
+        {
+
+            List<Shop> shops = new List<Shop>();
+            try
+            {
+                shops = _context.Shops
+                    .Where(s => s.ShopName == shopName).Select(s => s)
+                    .ToList();
+            }
+
+            catch (Exception ex)
+            {
+                shops = null;
+
+            }
+            return shops;
+        }
+
+
+
+        //Get Shop By City Name
+        public List<Shop> GetShopsByCityName(string cityName)
+        {
+
+            List<Shop> shops = new List<Shop>();
+            try
+            {
+                shops = _context.Shops
+                    .Where(s => s.CityName == cityName).Select(s => s)
+                    .ToList();
+            }
+
+            catch (Exception ex)
+            {
+                shops = null;
+
+            }
+            return shops;
+        }
+
+
+
+        //Get Shop By ProductName
+        public List<Shop> GetShopsByProduct(string productName)
+        {
+            List<Shop> shops = new List<Shop>();
+            try
+            {
+                Product product = _context.Products.Where(P => P.ProductName == productName).FirstOrDefault();
+
+                List<ShopProduct> ListShopsProducts = new List<ShopProduct>();
+                ListShopsProducts = _context.ShopProducts
+                                        .Where(sp => sp.ProductId == product.ProductId)
+                                        .GroupBy(sp => sp.ShopId)
+                                        .Select(g => g.First())
+                                        .ToList();
+
+
+
+                 
+                foreach (var shopProduct in ListShopsProducts)
+                {
+                    Shop shop = new Shop();
+                    shop = _context.Shops.Find(shopProduct.ShopId);
+                    if (shop != null)
+                    {
+                        shops.Add(shop);
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                shops = null;
+            }
+            return shops;
+        }
+
+
+
+
+        public List<Shop> GetShopByCategoryName(string categoryName)
+        {
+            
+            try
+            {
+                var category = _context.Categories.Where(c=>c.CategoryName==categoryName).FirstOrDefault();
+
+                var productIds = _context.Products.Where(p=>p.CategoryId == category.CategoryId).Select(p=>p.ProductId).ToList();
+                
+                List<int> shopsId = new List<int>();
+                foreach(var productId in productIds)
+                {
+                    var shopId = Convert.ToInt32(_context.ShopProducts.Where(sp=>sp.ProductId==productId).Select(sp=>sp.ShopId).FirstOrDefault());
+                    shopsId.Add(shopId);
+                }
+
+                List<Shop> shops = new List<Shop>();
+
+                foreach (var shopId in shopsId)
+                {
+                    var shop = _context.Shops.Where(sp => sp.ShopId == shopId).FirstOrDefault();
+                    shops.Add(shop);
+                }
+                return shops;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+
+        //Remove Shops By id
+        public int RemoveShop(int shopId)
+        {
+            int status = 0;
+            try
+            {
+                Shop shop = _context.Shops.Find(shopId);
+                if (shop != null)
+                {
+                    _context.Shops.Remove(shop);
+                    _context.SaveChanges();
+                    status = 1;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                status = -99;
+            }
+            return status;
+        }
+
+
+        //Update Operations on Shops
+        public bool UpdateShopContactNumber(int shopId, string contactNumber)
+        {
+            bool status = false;
+            try
+            {
+                Shop shop = _context.Shops.Find(shopId);
+                if (shop != null)
+                {
+                    shop.ContactNumber = contactNumber;
+                    _context.SaveChanges();
+                    status = true;
+                }
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+
+        }
+
+        //Change EmailId
+        public bool UpdateShopEmailId(int ShopId, string emailId)
+        {
+            bool status = false;
+            try
+            {
+                Shop shop = _context.Shops.Find(ShopId);
+                if (shop != null)
+                {
+                    shop.EmailId = emailId;
+                    _context.SaveChanges();
+                    status = true;
+                }
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        //Shop is open or not
+        public bool UpdateShopIsOpen(int ShopId, bool isOpen)
+        {
+            bool status = false;
+            try
+            {
+                Shop shop = _context.Shops.Find(ShopId);
+                if (shop != null)
+                {
+                    shop.IsOpen = isOpen;
+                    _context.SaveChanges();
+                    status = true;
+                }
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        #endregion
+
     }
+
 }
+
---
+++ FameFindsWebServices/Models/Shop.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FameFindsWebServices.Models
+{
+    public class Shop
+    {
+        [Key]
+        public int ShopId { get; set; }
+
+        [Required]
+        public string? ShopName { get; set; }
+
+        [Required]
+        public string EmailId { get; set; } = null!;
+
+        [Required]
+        public string CityName { get; set; } = null!;
+
+        [Required]
+        public string Pincode { get; set; } = null!;
+
+        [Required]
+        public string ContactNumber { get; set; } = null!;
+
+        [Required]
+        public string FullAddress { get; set; } = null!;
+
+        [Required]
+        public decimal Latitude { get; set; }
+
+        [Required]
+        public decimal Longitude { get; set; }
+
+        public TimeOnly? OpeningTime { get; set; }
+
+        public TimeOnly? ClosingTime { get; set; }
+
+        public bool? IsOpen { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        [Required]
+        public int VendorId { get; set; }
+    }
+}
