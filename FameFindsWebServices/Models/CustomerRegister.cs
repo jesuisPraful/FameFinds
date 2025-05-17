a42ea3ded108e43980a03d5205e96027235042c0
Pavan <pavanparjapnoar12@gmail.com>
2025-05-17 15:01:50 +0530

    Implemented PasswordHashing

---
+++ FameFindsWebServices/Models/CustomerRegister.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FameFindsWebServices.Models
+{
+    public class CustomerRegister
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+    }
+}
--- FameFindsWebServices/Program.cs
+++ FameFindsWebServices/Program.cs
@@ -1,5 +1,6 @@
 using FameFindsDAL;
 using FameFindsDAL.Models;
+using FameFindsWebServices.Services;
 using Microsoft.EntityFrameworkCore;
 
 internal class Program
@@ -17,6 +18,7 @@ internal class Program
 
         builder.Services.AddScoped<FameFindsRepository>();
         builder.Services.AddScoped<FameFindsContext>();
+        builder.Services.AddScoped<AuthenticationService>();
 
         var app = builder.Build();
 
