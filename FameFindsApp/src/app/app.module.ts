dee24936caa5c803ebb118c2b5e1268e8e4dd4de
Nheha-Reddy <135942823+Nheha-Reddy@users.noreply.github.com>
2025-06-10 19:53:17 +0530

    commit for pull

--- FameFindsApp/src/app/app.module.ts
+++ FameFindsApp/src/app/app.module.ts
@@ -11,13 +11,17 @@ import { RegisterService } from './services/register.service';
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { CustomerService } from './services/customer.service';
+import { LandingComponent } from './components/landing/landing.component';
+import { RegistrationPage2Component } from './components/registration-page2/registration-page2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewProductsComponent,
     RegistrationPageComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrationPage2Component,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
