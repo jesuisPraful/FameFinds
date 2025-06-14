6f3f9f07054eb9ac77df224f5a740869d201998e
jesuisPraful <prafusingh211@gmail.com>
2025-06-14 14:36:01 +0530

    City and Shops

--- FameFindsApp/src/app/app.module.ts
+++ FameFindsApp/src/app/app.module.ts
@@ -1,23 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductService } from './services/product.service';
-import { ViewProductsComponent } from './components/view-products/view-products.component';
-import { FormsModule } from '@angular/forms';
 import { RegisterService } from './services/register.service';
+import { CustomerService } from './services/customer.service';
+import { VendorService } from './services/vendor.service';
+import { ShopService } from './services/shop.service';
+import { ViewProductsComponent } from './components/view-products/view-products.component';
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
 import { LoginComponent } from './components/login/login.component';
-import { CustomerService } from './services/customer.service';
 import { LandingComponent } from './components/landing/landing.component';
-import { VendorService } from './services/vendor.service';
 import { LoginVendorComponent } from './components/login-vendor/login-vendor.component';
-import { RegistrationPage2Component } from './components/registration-page2/registration-page2.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 import { OtpVerificationComponent } from './components/otp-verification/otp-verification.component';
+import { ViewShopsComponent } from './components/view-shops/view-shops.component';
+import { ViewCitiesComponent } from './components/view-cities/view-cities.component';
+import { UpdateShopComponent } from './components/update-shop/update-shop.component';
+import { IsOpenComponent } from './components/is-open/is-open.component';
+import { RemoveShopComponent } from './components/remove-shop/remove-shop.component';
+import { AddCityComponent } from './components/add-city/add-city.component';
+import { RemoveCityComponent } from './components/remove-city/remove-city.component';
+import { AddShopsComponent } from './components/add-shops/add-shops.component';
 
 @NgModule({
   declarations: [
@@ -25,12 +32,19 @@ import { OtpVerificationComponent } from './components/otp-verification/otp-veri
     ViewProductsComponent,
     RegistrationPageComponent,
     LoginComponent,
-    RegistrationPage2Component,
     LandingComponent,
     LoginVendorComponent,
     ForgotPasswordComponent,
     ResetPasswordComponent,
-    OtpVerificationComponent
+    OtpVerificationComponent,
+    ViewShopsComponent,
+    ViewCitiesComponent,
+    UpdateShopComponent,
+    IsOpenComponent,
+    RemoveShopComponent,
+    AddCityComponent,
+    RemoveCityComponent,
+    AddShopsComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +52,13 @@ import { OtpVerificationComponent } from './components/otp-verification/otp-veri
     HttpClientModule,
     FormsModule
   ],
-  providers: [ProductService, RegisterService, CustomerService, VendorService],
+  providers: [
+    ProductService,
+    RegisterService,
+    CustomerService,
+    VendorService,
+    ShopService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ FameFindsApp/src/app/components/is-open/is-open.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IsOpenComponent } from './is-open.component';
+
+describe('IsOpenComponent', () => {
+  let component: IsOpenComponent;
+  let fixture: ComponentFixture<IsOpenComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ IsOpenComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(IsOpenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
