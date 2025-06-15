3ae36a0a75a35baccf717d5afbcd1a2f4af8c3c4
jesuisPraful <prafusingh211@gmail.com>
2025-06-15 12:23:57 +0530

    changes in repo  shop controller

--- FameFindsApp/src/app/app.module.ts
+++ FameFindsApp/src/app/app.module.ts
@@ -25,6 +25,9 @@ import { RemoveShopComponent } from './components/remove-shop/remove-shop.compon
 import { AddCityComponent } from './components/add-city/add-city.component';
 import { RemoveCityComponent } from './components/remove-city/remove-city.component';
 import { AddShopsComponent } from './components/add-shops/add-shops.component';
+import { ForgotPasswordVendorComponent } from './components/forgot-password-vendor/forgot-password-vendor.component';
+import { ForgotpasswordvendorService } from './services/forgotpasswordvendor.service';
+import { ForgotpasswordService } from './services/forgotpassword.service';
 
 @NgModule({
   declarations: [
@@ -44,7 +47,8 @@ import { AddShopsComponent } from './components/add-shops/add-shops.component';
     RemoveShopComponent,
     AddCityComponent,
     RemoveCityComponent,
-    AddShopsComponent
+    AddShopsComponent,
+    ForgotPasswordVendorComponent
   ],
   imports: [
     BrowserModule,
@@ -57,7 +61,9 @@ import { AddShopsComponent } from './components/add-shops/add-shops.component';
     RegisterService,
     CustomerService,
     VendorService,
-    ShopService
+    ShopService,
+    ForgotpasswordvendorService,
+    ForgotpasswordService
   ],
   bootstrap: [AppComponent]
 })
