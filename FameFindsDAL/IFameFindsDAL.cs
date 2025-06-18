7d76b28e4ba7c5f8bc92eeb9fbee5942c141b32b
Pavan <pavanparjapnoar12@gmail.com>
2025-06-18 11:51:14 +0530

    Final Commit

--- FameFindsDAL/IFameFindsDAL.cs
+++ FameFindsDAL/IFameFindsDAL.cs
@@ -27,7 +27,6 @@ namespace FameFindsDAL
         void MarkOtpAsUsed(int customerId, string otp);
         CustomerPasswordResetToken? GetLatestVerifiedOtp(string email);
 
-        int GetCustomerIdByEmail(string email);
         #endregion
 
         #region category
@@ -75,7 +74,6 @@ namespace FameFindsDAL
         List<ShopWithRatingDto> GetShopsSortedByRating();
         double GetAverageRatingByShopId(int shopId);
 
-        List<object> GetRatingsByVendor(int vendorId);
         #endregion
 
         #region shop
@@ -96,8 +94,6 @@ namespace FameFindsDAL
         bool UpdateShopContactNumber(int shopId, string nContactNumber, string contactNumber);
         bool UpdateShopEmailId(int ShopId, string nemailId, string emailId);
         bool UpdateShopIsOpen(int ShopId, bool isOpen);
-        double GetAverageRatingByShopId(int shopId);
-        List<int> GetShopIdsByVendorId(int vendorId);
 
         #endregion
 
@@ -109,8 +105,6 @@ namespace FameFindsDAL
         bool DeleteCity(int cityId);
         City GetCityById(int cityId);
         City GetCityByName(string cityName);
-        City CityByShop(Shop shop);
-        City CityByShopId(int shopId);
 
         #endregion
 
